ed6b386bf334a5ca9467b8ec4fc6dcca86216fdf
Apple <apple@Apples-MacBook-Pro.local>
2022-06-18 17:13:51 +0530

    new build

--- src/app/components/p-input/p-input.component.ts
+++ src/app/components/p-input/p-input.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { comapy_name } from 'src/app/app-constants';
-import { faArrowRight, faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faCheck, faL, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { InputBlockConfig } from 'src/app/domains/config-objects';
+
 
 @Component({
   selector: 'app-p-input',
@@ -10,16 +12,51 @@ import { faArrowRight, faCheck, faXmark } from '@fortawesome/free-solid-svg-icon
 export class PInputComponent implements OnInit {
   titleText = "Welcome to "+comapy_name+"!";
   subText =  "Lets create your personal space..";
-  emailLabel =  "Enter your email";
-  passwordLabel = "Create a password";
-  userNameLabel = "Enter a username";
+ 
   continueLabel = "Continue";
   faArrowRight = faArrowRight;
   faCheck = faCheck;
   faXmark = faXmark;
+
+  displayConfigs : InputBlockConfig[] = [
+    {
+      name: "email",
+      label: "Enter your email",
+      isVisible : true,
+      isContinueVisible: true,
+      inputStatus : [true,false,false]
+    },
+    {
+      name: "password",
+      label: "Create a password",
+      isVisible : false,
+      isContinueVisible: true,
+      inputStatus : [true,false,false]
+    },
+    {
+      name: "password",
+      label: "Enter a password",
+      isVisible : false,
+      isContinueVisible: true,
+      inputStatus : [true,false,false],
+    },
+    
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+  public continueService(index : number) {
+    let len = this.displayConfigs.length;
+    if(index < len-1){
+      this.displayConfigs[index+1].isVisible=true;
+      this.displayConfigs[index+1].isContinueVisible=true;
+      this.displayConfigs[index].isContinueVisible =false;
+    }
+  }
+
+
+  }
+
---
+++ src/app/domains/config-objects.ts
@@ -0,0 +1,7 @@
+export interface InputBlockConfig{
+    name: string;
+    label: string;
+    isVisible : boolean;
+    isContinueVisible: boolean;
+    inputStatus : boolean[];
+}
